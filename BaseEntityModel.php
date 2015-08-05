f4d75c19bdc4cc4d228e787914ab1dc16ff788fe
Zhao vistart <i@vistart.name>
2015-08-05 19:48:19 +0800

    Supplement Notes & Add Pointers.
    Supplement notes of usage.
    Add pointers to attributes' name.

--- BaseEntityModel.php
+++ BaseEntityModel.php
@@ -20,11 +20,33 @@ use yii\db\ActiveRecord;
  * This Base Entity Model is used for being extended to the other Active Record, 
  * which include `createdAt`, `updatedAt`, `ipAddress`, `ipType`. `GUID` 
  * attributes, and so on. The `createdAt` and `updatedAt` attributes' meaning 
- * are same as the DatetimeBehavior.
+ * are same as those of DatetimeBehavior.
  * The `ipAddress` store the standard IP Address, which is of IPv4 or IPv6.
  * You should set the IP Address before using it. the setIpAddress() method
  * will judge the IP Address type automatically.
  * The `GUID` is used for the primary key of this model.
+ * 
+ * You should extend this class, for your own ActiveRecord class(es). Then you
+ * need to specify the following attributes' name:
+ * guidAttribute
+ * createdAtAttribute
+ * updatedAtAttribute
+ * ipAttribute1
+ * ipAttribute2
+ * ipAttribute3
+ * ipAttribute4
+ * ipTypeAttribute
+ * 
+ * The above attributes' name also have the default name.
+ * 
+ * @property string $guidAttribute the attribute name of GUID attribute (primary key).
+ * @property string $createdAtAttribute
+ * @property string $updatedAtAttribute
+ * @property string $ipAttribute1
+ * @property string $ipAttribute2
+ * @property string $ipAttribute3
+ * @property string $ipAttribute4
+ * @property string $ipTypeAttribute
  * 
  * @property string $ipAddress IP Address. You should get before setting it.
  * 
@@ -33,7 +55,17 @@ use yii\db\ActiveRecord;
 class BaseEntityModel extends ActiveRecord
 {
     public $guidAttribute = 'guid';
+    
+    /**
+     * @var string the attribute that will receive datetime value
+     * Set this property to false if you do not want to record the creation time.
+     */
     public $createdAtAttribute = 'create_time';
+    
+    /**
+     * @var string the attribute that will receive datetime value.
+     * Set this property to false if you do not want to record the update time.
+     */
     public $updatedAtAttribute = 'update_time';
     public $ipAttribute1 = 'ip_1';
     public $ipAttribute2 = 'ip_2';
@@ -49,9 +81,15 @@ class BaseEntityModel extends ActiveRecord
         parent::init();
     }
     
+    /**
+     * Initialize the default value of all attributes.
+     * You should override this method to specify the above Attribute Name
+     * attributes, and call the parent's method in the end of your own.
+     */
     protected function initDefaultValues()
     {
-        $this->$this->guidAttribute = self::GenerateUuid();
+        $guidAttribute = $this->guidAttribute;
+        $this->$guidAttribute = self::GenerateUuid();
         $this->ipAddress = Yii::$app->request->userIP;
     }
     
@@ -92,31 +130,36 @@ class BaseEntityModel extends ActiveRecord
         $ipAttribute2 = $this->ipAttribute2;
         $ipAttribute3 = $this->ipAttribute3;
         $ipAttribute4 = $this->ipAttribute4;
-        if ($this->$this->ipTypeAttribute == Ip::IPv4){
-            return Ip::long2ip($this->$this->ipAttribute4);
+        if ($this->$ipTypeAttribute == Ip::IPv4){
+            return Ip::long2ip($this->$ipAttribute4);
         }
-        if ($this->$this->ipTypeAttribute == Ip::IPv6){
-            return Ip::LongtoIPv6(Ip::populateIPv6([$this->$this->ipAttribute1, $this->$this->ipAttribute2, $this->$this->ipAttribute3, $this->$this->ipAttribute4]));
+        if ($this->$ipTypeAttribute == Ip::IPv6){
+            return Ip::LongtoIPv6(Ip::populateIPv6([$this->$ipAttribute1, $this->$ipAttribute2, $this->$ipAttribute3, $this->$ipAttribute4]));
         }
-        return $this->$this->ipAttribute4;
+        return $this->$ipAttribute4;
     }
     
     public function setIpAddress($ip)
     {
-        $this->$this->ipTypeAttribute = Ip::judgeIPtype($ip);
-        if ($this->$this->ipTypeAttribute == Ip::IPv4){
-            $this->$this->ipAttribute1 = 0;
-            $this->$this->ipAttribute2 = 0;
-            $this->$this->ipAttribute3 = 0;
-            $this->$this->ipAttribute4 = Ip::ip2long($ip);
+        $ipTypeAttribute = $this->ipTypeAttribute;
+        $ipAttribute1 = $this->ipAttribute1;
+        $ipAttribute2 = $this->ipAttribute2;
+        $ipAttribute3 = $this->ipAttribute3;
+        $ipAttribute4 = $this->ipAttribute4;
+        $this->$ipTypeAttribute = Ip::judgeIPtype($ip);
+        if ($this->$ipTypeAttribute == Ip::IPv4){
+            $this->$ipAttribute1 = 0;
+            $this->$ipAttribute2 = 0;
+            $this->$ipAttribute3 = 0;
+            $this->$ipAttribute4 = Ip::ip2long($ip);
             return Ip::IPv4;
         }
-        if ($this->$this->ipTypeAttribute == Ip::IPv6){
+        if ($this->$ipTypeAttribute == Ip::IPv6){
             $ips = Ip::splitIPv6(Ip::IPv6toLong($ip));
-            $this->$this->ipAttribute1 = bindec($ips[0]);
-            $this->$this->ipAttribute2 = bindec($ips[1]);
-            $this->$this->ipAttribute3 = bindec($ips[2]);
-            $this->$this->ipAttribute4 = bindec($ips[3]);
+            $this->$ipAttribute1 = bindec($ips[0]);
+            $this->$ipAttribute2 = bindec($ips[1]);
+            $this->$ipAttribute3 = bindec($ips[2]);
+            $this->$ipAttribute4 = bindec($ips[3]);
             return Ip::IPv6;
         }
         return $ip;
