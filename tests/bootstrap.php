b98843707c325b413076fa450cbebaaee25207e1
Zhao vistart <i@vistart.name>
2016-01-04 16:47:02 +0800

    2.0.0.20160104-nightly-patch-testconfig
    2.0.0.20160104-nightly-patch-testconfig

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -18,8 +18,6 @@ define('YII_DEBUG', true);
 $_SERVER['SCRIPT_NAME'] = '/' . __DIR__;
 $_SERVER['SCRIPT_FILENAME'] = __FILE__;
 
-require_once(__DIR__ . '/../../../../vendor/autoload.php');
-require_once(__DIR__ . '/../../../../vendor/yiisoft/yii2/Yii.php');
+require_once(__DIR__ . '/../vendor/autoload.php');
+require_once(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
-Yii::setAlias('@app', (__DIR__ . '/../../../..'));
-//Yii::setAlias('@vistart/Models', dirname(__DIR__));
