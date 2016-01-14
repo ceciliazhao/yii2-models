29ae991064db7039e3b775a27b489837539169ac
Zhao vistart <i@vistart.name>
2016-01-14 17:23:16 +0800

    2.0.0.20160114-nightly-database-fix
    change database structure.

--- tests/data/yii2-models.sql
+++ tests/data/yii2-models.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: 2016-01-12 01:42:41
+-- Generation Time: 2016-01-14 17:22:10
 -- 服务器版本： 5.7.10
 -- PHP Version: 5.6.17
 
@@ -28,11 +28,11 @@ USE `yii2-models`;
 -- 表的结构 `user`
 --
 -- 创建时间： 2016-01-11 12:58:55
--- 最后更新： 2016-01-11 17:29:20
+-- 最后更新： 2016-01-14 09:14:45
 --
 
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
+CREATE TABLE IF NOT EXISTS `user` (
   `guid` varchar(36) NOT NULL,
   `id` int(14) UNSIGNED NOT NULL DEFAULT '0',
   `pass_hash` varchar(80) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '',
@@ -47,7 +47,11 @@ CREATE TABLE `user` (
   `access_token` varchar(40) NOT NULL DEFAULT '',
   `password_reset_token` varchar(40) NOT NULL DEFAULT '',
   `status` tinyint(3) UNSIGNED NOT NULL DEFAULT '0',
-  `source` varchar(255) NOT NULL DEFAULT '0'
+  `source` varchar(255) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`guid`),
+  UNIQUE KEY `user_id_unique` (`id`),
+  UNIQUE KEY `user_auth_key_unique` (`auth_key`) USING BTREE,
+  UNIQUE KEY `user_access_token_unique` (`access_token`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -56,18 +60,20 @@ CREATE TABLE `user` (
 -- 表的结构 `user_additional_account`
 --
 -- 创建时间： 2016-01-11 17:21:48
--- 最后更新： 2016-01-11 17:29:13
+-- 最后更新： 2016-01-14 09:14:38
 --
 
 DROP TABLE IF EXISTS `user_additional_account`;
-CREATE TABLE `user_additional_account` (
+CREATE TABLE IF NOT EXISTS `user_additional_account` (
   `guid` varchar(36) NOT NULL,
   `user_guid` varchar(36) NOT NULL,
   `id` varchar(8) NOT NULL,
   `pass_hash` varchar(60) NOT NULL DEFAULT '',
   `enable_login` tinyint(3) UNSIGNED NOT NULL DEFAULT '0',
   `create_time` datetime NOT NULL DEFAULT '1970-01-01 00:00:00',
-  `update_time` datetime NOT NULL DEFAULT '1970-01-01 00:00:00'
+  `update_time` datetime NOT NULL DEFAULT '1970-01-01 00:00:00',
+  PRIMARY KEY (`guid`),
+  UNIQUE KEY `user_email_id_unique` (`user_guid`,`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -75,12 +81,12 @@ CREATE TABLE `user_additional_account` (
 --
 -- 表的结构 `user_email`
 --
--- 创建时间： 2016-01-11 12:59:04
--- 最后更新： 2016-01-11 17:29:13
+-- 创建时间： 2016-01-14 08:30:22
+-- 最后更新： 2016-01-14 09:14:38
 --
 
 DROP TABLE IF EXISTS `user_email`;
-CREATE TABLE `user_email` (
+CREATE TABLE IF NOT EXISTS `user_email` (
   `guid` varchar(36) NOT NULL,
   `user_guid` varchar(36) NOT NULL,
   `id` varchar(8) NOT NULL,
@@ -90,35 +96,42 @@ CREATE TABLE `user_email` (
   `update_time` datetime NOT NULL DEFAULT '1970-01-01 00:00:00',
   `confirmed` tinyint(3) UNSIGNED NOT NULL DEFAULT '0',
   `confirm_time` datetime NOT NULL DEFAULT '1970-01-01 00:00:00',
-  `confirm_code` varchar(8) NOT NULL
+  `confirm_code` varchar(8) NOT NULL,
+  PRIMARY KEY (`guid`),
+  UNIQUE KEY `user_email_id_unique` (`user_guid`,`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Indexes for dumped tables
---
+-- --------------------------------------------------------
 
 --
--- Indexes for table `user`
+-- 表的结构 `user_relation`
 --
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`guid`),
-  ADD UNIQUE KEY `user_id_unique` (`id`),
-  ADD UNIQUE KEY `user_auth_key_unique` (`auth_key`) USING BTREE,
-  ADD UNIQUE KEY `user_access_token_unique` (`access_token`);
+-- 创建时间： 2016-01-14 08:56:26
+-- 最后更新： 2016-01-14 09:14:45
+--
 
---
--- Indexes for table `user_additional_account`
---
-ALTER TABLE `user_additional_account`
-  ADD PRIMARY KEY (`guid`),
-  ADD UNIQUE KEY `user_email_id_unique` (`user_guid`,`id`);
-
---
--- Indexes for table `user_email`
---
-ALTER TABLE `user_email`
-  ADD PRIMARY KEY (`guid`),
-  ADD UNIQUE KEY `user_email_id_unique` (`user_guid`,`id`);
+DROP TABLE IF EXISTS `user_relation`;
+CREATE TABLE IF NOT EXISTS `user_relation` (
+  `guid` varchar(36) NOT NULL,
+  `user_guid` varchar(36) NOT NULL,
+  `remark` varchar(255) NOT NULL DEFAULT '',
+  `other_guid` varchar(36) NOT NULL,
+  `type` tinyint(3) UNSIGNED NOT NULL DEFAULT '0',
+  `favorite` tinyint(3) UNSIGNED NOT NULL DEFAULT '0',
+  `ip_1` int(10) UNSIGNED NOT NULL DEFAULT '0',
+  `ip_2` int(10) UNSIGNED NOT NULL DEFAULT '0',
+  `ip_3` int(10) UNSIGNED NOT NULL DEFAULT '0',
+  `ip_4` int(10) UNSIGNED NOT NULL DEFAULT '0',
+  `ip_type` tinyint(3) UNSIGNED NOT NULL DEFAULT '4',
+  `create_time` datetime NOT NULL DEFAULT '1970-01-01 00:00:00',
+  `update_time` datetime NOT NULL DEFAULT '1970-01-01 00:00:00',
+  `confirmed` tinyint(3) UNSIGNED NOT NULL DEFAULT '0',
+  `confirm_time` datetime NOT NULL DEFAULT '1970-01-01 00:00:00',
+  `groups` text NOT NULL,
+  PRIMARY KEY (`guid`),
+  UNIQUE KEY `user_other_guid_unique` (`user_guid`,`other_guid`),
+  KEY `relation_other_guid_fkey` (`other_guid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- 限制导出的表
@@ -134,7 +147,14 @@ ALTER TABLE `user_additional_account`
 -- 限制表 `user_email`
 --
 ALTER TABLE `user_email`
-  ADD CONSTRAINT `user_guid_fkey` FOREIGN KEY (`user_guid`) REFERENCES `user` (`guid`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `email_user_guid_fkey` FOREIGN KEY (`user_guid`) REFERENCES `user` (`guid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- 限制表 `user_relation`
+--
+ALTER TABLE `user_relation`
+  ADD CONSTRAINT `relation_other_guid_fkey` FOREIGN KEY (`other_guid`) REFERENCES `user` (`guid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `relation_user_guid_fkey` FOREIGN KEY (`user_guid`) REFERENCES `user` (`guid`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
