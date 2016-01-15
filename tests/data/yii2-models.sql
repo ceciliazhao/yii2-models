b746f054a5c924c105aa38a60bc1c76fef3dee75
Zhao vistart <i@vistart.name>
2016-01-15 23:11:43 +0800

    2.0.0.20160115-nightly-patch4-fix
    database structure fixed.

--- tests/data/yii2-models.sql
+++ tests/data/yii2-models.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: 2016-01-14 17:22:10
+-- Generation Time: 2016-01-15 23:10:59
 -- 服务器版本： 5.7.10
 -- PHP Version: 5.6.17
 
@@ -28,7 +28,7 @@ USE `yii2-models`;
 -- 表的结构 `user`
 --
 -- 创建时间： 2016-01-11 12:58:55
--- 最后更新： 2016-01-14 09:14:45
+-- 最后更新： 2016-01-15 14:55:22
 --
 
 DROP TABLE IF EXISTS `user`;
@@ -60,7 +60,7 @@ CREATE TABLE IF NOT EXISTS `user` (
 -- 表的结构 `user_additional_account`
 --
 -- 创建时间： 2016-01-11 17:21:48
--- 最后更新： 2016-01-14 09:14:38
+-- 最后更新： 2016-01-15 14:55:00
 --
 
 DROP TABLE IF EXISTS `user_additional_account`;
@@ -82,7 +82,7 @@ CREATE TABLE IF NOT EXISTS `user_additional_account` (
 -- 表的结构 `user_email`
 --
 -- 创建时间： 2016-01-14 08:30:22
--- 最后更新： 2016-01-14 09:14:38
+-- 最后更新： 2016-01-15 14:55:00
 --
 
 DROP TABLE IF EXISTS `user_email`;
@@ -106,8 +106,8 @@ CREATE TABLE IF NOT EXISTS `user_email` (
 --
 -- 表的结构 `user_relation`
 --
--- 创建时间： 2016-01-14 08:56:26
--- 最后更新： 2016-01-14 09:14:45
+-- 创建时间： 2016-01-15 11:31:11
+-- 最后更新： 2016-01-15 14:55:22
 --
 
 DROP TABLE IF EXISTS `user_relation`;
@@ -125,12 +125,30 @@ CREATE TABLE IF NOT EXISTS `user_relation` (
   `ip_type` tinyint(3) UNSIGNED NOT NULL DEFAULT '4',
   `create_time` datetime NOT NULL DEFAULT '1970-01-01 00:00:00',
   `update_time` datetime NOT NULL DEFAULT '1970-01-01 00:00:00',
-  `confirmed` tinyint(3) UNSIGNED NOT NULL DEFAULT '0',
-  `confirm_time` datetime NOT NULL DEFAULT '1970-01-01 00:00:00',
   `groups` text NOT NULL,
   PRIMARY KEY (`guid`),
   UNIQUE KEY `user_other_guid_unique` (`user_guid`,`other_guid`),
   KEY `relation_other_guid_fkey` (`other_guid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `user_relation_group`
+--
+-- 创建时间： 2016-01-15 13:54:14
+-- 最后更新： 2016-01-15 14:55:22
+--
+
+DROP TABLE IF EXISTS `user_relation_group`;
+CREATE TABLE IF NOT EXISTS `user_relation_group` (
+  `guid` varchar(36) NOT NULL,
+  `user_guid` varchar(36) NOT NULL,
+  `content` varchar(255) NOT NULL DEFAULT '' COMMENT 'group name',
+  `update_time` datetime NOT NULL DEFAULT '1970-01-01 00:00:00',
+  `create_time` datetime NOT NULL DEFAULT '1970-01-01 00:00:00',
+  PRIMARY KEY (`guid`),
+  KEY `relation_group_user_guid_fkey` (`user_guid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -155,6 +173,12 @@ ALTER TABLE `user_email`
 ALTER TABLE `user_relation`
   ADD CONSTRAINT `relation_other_guid_fkey` FOREIGN KEY (`other_guid`) REFERENCES `user` (`guid`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `relation_user_guid_fkey` FOREIGN KEY (`user_guid`) REFERENCES `user` (`guid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- 限制表 `user_relation_group`
+--
+ALTER TABLE `user_relation_group`
+  ADD CONSTRAINT `relation_group_user_guid_fkey` FOREIGN KEY (`user_guid`) REFERENCES `user` (`guid`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
