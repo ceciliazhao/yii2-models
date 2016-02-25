63ae5ebe030802fa9f8d252d0b5c3e78f4921282
Zhao vistart <i@vistart.name>
2016-02-25 16:40:18 +0800

    update array.

--- traits/BlameableTrait.php
+++ traits/BlameableTrait.php
@@ -269,7 +269,12 @@ trait BlameableTrait
             TagDependency::invalidate($cache, [$this->getEntityRulesCacheTag()]);
         }
         $rules = array_merge(
-            parent::rules(), $this->getConfirmationRules(), $this->getBlameableAttributeRules(), $this->getDescriptionRules(), $this->getContentRules(), $this->getSelfBlameableRules()
+            parent::rules(),
+            $this->getConfirmationRules(),
+            $this->getBlameableAttributeRules(),
+            $this->getDescriptionRules(),
+            $this->getContentRules(),
+            $this->getSelfBlameableRules()
         );
         $this->setBlameableRules($rules);
         return $this->blameableLocalRules;
